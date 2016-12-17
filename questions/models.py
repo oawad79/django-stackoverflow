ab1eae5928d2adb8e13406015db23a42fb087d8f
oawad <oawad79@gmail.com>
2016-12-17 17:05:11 -0600

    req.txt

--- questions/models.py
+++ questions/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Question(models.Model):
     question_title = models.CharField(max_length=200, default='')
     question_text = models.CharField(max_length=1000)
+    views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.question_title
