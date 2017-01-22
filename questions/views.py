b238bf109b965230a31523b7146b9de87270f88b
Osama Awad <oawad79@gmail.com>
2017-01-21 21:55:35 -0600

    organized imports

--- questions/views.py
+++ questions/views.py
@@ -194,3 +194,20 @@ def add_user(request):
               a.save()
               return redirect('questions:index')
     return render(request, 'registration.html', {'form': form,})
+
+@login_required
+def change_password(request):
+    newpassword = request.POST.get("newpassword")
+    renewpasssword = request.POST.get("renewpasssword")
+    username=request.user.username
+    if newpassword == renewpasssword:
+        if request.method == 'GET':
+            form = ChangePasswordForm()
+        else:
+            u = User.objects.get(username__exact=username)
+            u.set_password(newpassword)
+            u.save()
+            return redirect('/login')
+    else:
+        return redirect('changepassword')
+    return render(request, 'registration/password_change_form.html', {'form': form, })
